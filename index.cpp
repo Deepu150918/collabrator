ab1e4035d8bfa7c8a4e9ad4f08210c6b55f160a0
Deepu <deep@example.com>
2026-06-25 17:16:24 +0530

    Addiing diff function

--- index.cpp
+++ index.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-inline int sum(int a , int b){
+inline int sum(int a, int b) {
   return a + b;
 }
 
+inline int diff(int a, int b) {
+  return a - b;
+}
+
 int main() {
+  std::cout << "diff of " << diff(5, 9) << std::endl;
   return 0;
 }
